d5b2640a7fcb2d5d1cab9b6654139233f4912b2c
WillianChavez <williamchavezamaya@gmail.com>
2022-10-07 22:46:44 -0600

    rediseño de actualizar administradores, cambios en el controlador y en el formulario

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -51,61 +51,70 @@ const adminController = {
             }
         } catch (err) {
             console.log(err)
+
+            req.flash('error_msg', 'Ah ocurrido un error')
             res.status(500).json(err)
         }
     },
     //permite eliminar un administrador de la base de datos
     delete: async (req, res) => {
-        try{
+        try {
             let id = req.params.id
-            const user= await Usuario.findOne({ where: { id:id }})
+            const user = await Usuario.findOne({ where: { id: id } })
             await user.destroy()
+
+            req.flash('success_msg', 'Administrador eliminado correctamente')
             res.redirect('/admins')
-        }catch(err){
+        } catch (err) {
             console.log(err)
+
+            req.flash('error_msg', 'Lo siento, ha ocurrido un error al momento de eliminar el administrador')
             res.redirect('/admins')
         }
-
     },
     //permite editar un administrador de la base de datos
     edit: async (req, res) => {
-        try{
+        try {
             let id = req.params.id
-            const user= await Administrador.findOne({ 
-                where: { 
-                    UsuarioId:id 
+            const user = await Administrador.findOne({
+                where: {
+                    UsuarioId: id,
                 },
-                raw:true
-            })           
-            res.render('administrador/edit', {user})
-        }catch(err){
+                raw: true,
+            })
+            res.render('administrador/edit', { user })
+        } catch (err) {
             console.log(err)
             res.redirect('/admins')
         }
     },
     //permite actualizar un administrador de la base de datos
     update: async (req, res) => {
-        try{
+        try {
+            console.log('hola')
             let id = req.params.id
             const { alias, email, password, nombre, apellido, dui, telefono } = req.body
-            const user= await Usuario.findOne({ where: { id:id }})
-            user.nombre= alias
-            user.email= email
-            user.password= password
+            const user = await Usuario.findOne({ where: { id: id } })
+            user.nombre = alias
+            user.email = email
+            user.password = password
             await user.save()
-            const admin= await Administrador.findOne({ where: { UsuarioId:id }})
-            admin.nombre= nombre
-            admin.apellido= apellido
-            admin.dui= dui
-            admin.telefono= telefono
+            const admin = await Administrador.findOne({ where: { UsuarioId: id } })
+            admin.nombre = nombre
+            admin.apellido = apellido
+            admin.dui = dui
+            admin.telefono = telefono
             await admin.save()
-            res.render('/admins')
-        }catch(err){
+
+            req.flash('success_msg', 'Administrador actualizado correctamente')
+            res.redirect('/admins')
+        } catch (err) {
             console.log(err)
+
+            req.flash('error_msg', 'No es posible actualizar el administrador')
             res.redirect('/admins')
         }
-    }
-    
+    },
 }
 
 module.exports = adminController
