15c10de36be04a0aae118998bbf82b70da3decee
SilviaPerezz <pb21015@ues.edu.sv>
2022-10-12 14:04:26 -0600

    modificaciones institucion

--- src/migrations/20221012015136-create-institucion.js
+++ src/migrations/20221012015136-create-institucion.js
@@ -15,7 +15,7 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
-      idInstitucion: {
+      direccion: {
         type: Sequelize.STRING
       },
       createdAt: {
