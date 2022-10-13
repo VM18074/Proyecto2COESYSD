fed756d278498554adf6a7508a2bfae7e3946331
WillianChavez <williamchavezamaya@gmail.com>
2022-10-12 21:23:11 -0600

    Cambios en modelo Rol

--- src/models/rol.js
+++ src/models/rol.js
@@ -19,6 +19,8 @@ module.exports = (sequelize, DataTypes) => {
         {
             nombre: {
                 type: DataTypes.STRING,
+                allowNull: false,
+                primaryKey: true,
             },
         },
         {
