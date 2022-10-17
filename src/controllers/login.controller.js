6943e01efad959ea63d992d34aff3f91e2c2c56f
VaneHer <92071026+VaneHer@users.noreply.github.com>
2022-10-17 14:28:12 -0300

    Login
    Verificar Datos

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,10 +1,54 @@
 const { Usuario } = require('./../models/')
+const bcryptjs = require('bcryptjs')
 
-const loginController={
-    index:(req, res) => {   
+const loginController= {
+    index: async (req, res) => {   
     res.render('login/index')
     },
 
-}                                                                                                                          
+    
+    login:async  (req, res)=>{
+        if(req.session.loggedin != true){
+            res.render('login/index')             
+        }
+
+    },
+
+
+    // permite agregar un nuevo usuario a la base de datos
+    outh: async (req, res) => {
+        try {
+            const data = req.body
+            const { email, password} = req.body
+
+            const oldUser = await Usuario.findOne({
+                where: {
+                    email,
+                },
+            })
+
+            if (oldUser ) {
+                         bcryptjs.compare(data.password, oldUser.password(err, isMatch));{
+                        if(!isMatch){
+                            res.render('/')
+                            return
+                        } 
+                    }
+            }
+                    else{
+                req.flash('error_msg', 'No existe un Usuario con ese email.')
+                res.redirect('/')
+                return }
+                
+            
+        } catch (err) {
+        console.log(err)
+
+        req.flash('error_msg', 'Ha ocurrido un error')
+        res.status(500).json(err)
+    }
+    },
+}
+                                                                                                                        
 
 module.exports = loginController
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -3,5 +3,7 @@ const loginController = require('../controllers/login.controller')
 const router = Router()
 
 router.get('/login', loginController.index)
+router.post('/login/add', loginController.index)
+router.get('/login', loginController.login)
 
 module.exports = router
