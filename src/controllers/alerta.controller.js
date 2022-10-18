3534a358ddf6a5e7fddeb21a0e0d89adcc7bd1ee
VM18074 <vm18074@ues.edu.sv>
2022-10-18 14:19:48 -0600

    Update alerta.controller.js

--- src/controllers/alerta.controller.js
+++ src/controllers/alerta.controller.js
@@ -42,12 +42,6 @@ const alertaController = {
             raw: true,
                 nest: true,
             })
-<<<<<<< HEAD
-            req.session.loggedin=true;
-            req.session.admin=true;
- 
-         res.render('alerta/admin/index', {dataRows:data, admin: req.session.admin,logueado: req.session.loggedin })
-=======
        
             if(req.session.loggedin==true && req.session.admin==true){
                 res.render('alerta/admin/index', {dataRows:data})
@@ -55,7 +49,6 @@ const alertaController = {
                 res.render('home/index')
             }
          
->>>>>>> 0670d8d97d3d7b07abe38d9ee1357f3202a975fc
      },
  
 
