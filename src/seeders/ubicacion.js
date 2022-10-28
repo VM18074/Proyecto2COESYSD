53a7568d80f41618b957765d4f74683e92833ecd
WillianChavez <williamchavezamaya@gmail.com>
2022-10-27 23:16:31 -0600

    Agregando departamentos a seeder de ubicacion

--- src/seeders/ubicacion.js
+++ src/seeders/ubicacion.js
@@ -5,15 +5,31 @@ const { faker } = require('@faker-js/faker')
 module.exports = {
     async up(queryInterface, Sequelize) {
         let ubicacion = []
+        const departamentos = [
+            'Ahuachapán',
+            'Cabañas',
+            'Chalatenango',
+            'Cuscatlán',
+            'La Libertad',
+            'La Paz',
+            'La Unión',
+            'Morazán',
+            'San Miguel',
+            'San Salvador',
+            'San Vicente',
+            'Santa Ana',
+            'Sonsonate',
+            'Usulután',
+        ]
         for (let i = 0; i < 30; i++) {
             ubicacion = [
                 ...ubicacion,
                 {
                     nombre: faker.lorem.word(),
-                    departamento: faker.address.state(),
+                    departamento: departamentos[Math.floor(Math.random() * 14)],
                     municipio: faker.address.city(),
                     coordenada: faker.address.latitude(),
-                    alertumId: i+1,
+                    alertumId: i + 1,
                     createdAt: new Date(),
                     updatedAt: new Date(),
                 },
