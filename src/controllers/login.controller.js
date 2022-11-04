6ad7bf850d16562cc6bb367b5aad2fb833ae730b
Jefferson Alberto Arévalo Serrano <jeffer.arevalo8@gmail.com>
2022-11-03 21:34:53 -0600

    Graficas Municipios y

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,22 +1,58 @@
-const { Usuario } = require('./../models/')
+const { Usuario, Alerta, Daño, Medida, Ubicacion } = require('./../models/')
 const bcryptjs = require('bcryptjs')
 
-const loginController= {
-    index: async (req, res) => {  
-        if(req.session.loggedin != true){
-            res.render('login/index')             
-        } 
-    res.render('home/index')
+const loginController = {
+    index: async (req, res) => {
+        if (req.session.loggedin != true) {
+            res.render('login/index')
+        }
+            const data = await Alerta.findAll({
+                where: {
+                    activo: true,
+                },
+                include: [
+                    {
+                        model: Daño,
+                    },
+                    {
+                        model: Medida,
+                    },
+                    {
+                        model: Ubicacion,
+                    },
+                ],
+                raw: true,
+                nest: true,
+            })
+            res.render('home/index', { dataRows: data })
     },
 
-    logout:async  (req, res)=>{
-        
+    logout: async (req, res) => {
+
         if (req.session.loggedin == true) {
             req.session.destroy();
             console.log('Sesion cerrada')
-          }
-          //req.session.loggedin == null;
-          res.render('home/index');
+        }
+        const data = await Alerta.findAll({
+            where: {
+                activo: true,
+            },
+            include: [
+                {
+                    model: Daño,
+                },
+                {
+                    model: Medida,
+                },
+                {
+                    model: Ubicacion,
+                },
+            ],
+            raw: true,
+            nest: true,
+        })
+
+        res.render('home/index', { dataRows: data })
 
     },
 
@@ -25,7 +61,7 @@ const loginController= {
     auth: async (req, res) => {
         try {
             const data = req.body
-           // const { email, password} = req.body
+            // const { email, password} = req.body
 
             const oldUser = await Usuario.findOne({
                 where: {
@@ -33,39 +69,59 @@ const loginController= {
                 },
             })
 
-            if (oldUser ) {
-                         bcryptjs.compare(data.password, oldUser.password,(err, isMatch)=>{
-                        if(!isMatch){
-                            req.flash('error_msg', 'La contraseña no coincide')
-                            res.redirect('login')
-                            
-                            return
-                        } else{
-                            if(oldUser.RolNombre!=null){
-                                req.session.loggedin=true;
-                                req.session.admin=true;
-                                res.render('home/index', {logueado: req.session.loggedin, admin: req.session.admin})
-                            }else{
-                                req.session.loggedin=true;
-                                req.session.admin=null;
-                                res.render('home/index', {logueado: req.session.loggedin, admin: req.session.admin})
-                            }
-                            
+            if (oldUser) {
+                bcryptjs.compare(data.password, oldUser.password, async (err, isMatch) => {
+                    const data = await Alerta.findAll({
+                        where: {
+                            activo: true,
+                        },
+                        include: [
+                            {
+                                model: Daño,
+                            },
+                            {
+                                model: Medida,
+                            },
+                            {
+                                model: Ubicacion,
+                            },
+                        ],
+                        raw: true,
+                        nest: true,
+                    })
+            
+                    if (!isMatch) {
+                        req.flash('error_msg', 'La contraseña no coincide')
+                        res.redirect('login')
+
+                        return
+                    } else {
+                        if (oldUser.RolNombre != null) {
+                            req.session.loggedin = true;
+                            req.session.admin = true;
+                            res.render('home/index', { logueado: req.session.loggedin, admin: req.session.admin, dataRows: data })
+                        } else {
+                            req.session.loggedin = true;
+                            req.session.admin = null;
+                            res.render('home/index', { logueado: req.session.loggedin, admin: req.session.admin, dataRows: data })
                         }
-                    });
+
+                    }
+                });
             }
-                    else{
+            else {
                 req.flash('error_msg', 'No existe un Usuario con ese email.')
                 res.redirect('login')
-                return }
-                            
-        } catch (err) {
-        console.log(err)
+                return
+            }
 
-        req.flash('error_msg', 'Ha ocurrido un error')
-        res.status(500).json(err)
-    }
+        } catch (err) {
+            console.log(err)
+
+            req.flash('error_msg', 'Ha ocurrido un error')
+            res.status(500).json(err)
+        }
     },
 }
-                                                                                                                        
+
 module.exports = loginController
