7ff953d6a408c04ee3e49ad26a44267b4a7e6a4f
Andi7110 <mp21079@ues.edu.sv>
2022-11-18 19:43:02 -0600

    Nuevos correos Institucion

--- src/seeders/20221012154106-institucion.js
+++ src/seeders/20221012154106-institucion.js
@@ -19,7 +19,7 @@ module.exports = {
              {
                  name: 'Cruz Roja',
                  direccion:'PR44+R6F, San Salvador' ,
-                 email:'http://www.cruzrojasal.org.sv/',
+                 email:'relaciones.publicas@cruzrojasal.org.sv',
                  createdAt: new Date(),
                  updatedAt: new Date(),
              },
@@ -30,7 +30,7 @@ module.exports = {
              {
                  name: 'Cuerpo de Bomberos',
                  direccion:'Bo Sta Anita Cl Francisco Menéndez No 552 San Salvador - San Salvador.',
-                 email: 'http://www.bomberos.gob.sv/',
+                 email: 'info@patronatocbes.org',
 
                  createdAt: new Date(),
                  updatedAt: new Date(),
@@ -42,7 +42,7 @@ module.exports = {
              {
                  name:'Proteccion Civil de El Salvador',
                  direccion: 'Novena Calle Poniente y Quince Avenida Norte, Centro de Gobierno, San Salvador.',
-                 email: 'http://proteccioncivil.gob.sv/',
+                 email: 'prensa@proteccioncivil.gob.sv',
 
                  createdAt: new Date(),
                  updatedAt: new Date(),
@@ -53,8 +53,8 @@ module.exports = {
              ...inst,
              {
                  name:'Cruz Verde' ,
-                 direccion:'PQFJ+7PG, Calle Constitucion, Mejicanos' ,
-                 email: 'http://www.cruzverdesv.org/',
+                 direccion:'Avenida Bernal y Calle Constitución, Colonia Satélite, Casa #19, San Salvador, El Salvador' ,
+                 email: 'info@cruzverdesv.org',
 
                  createdAt: new Date(),
                  updatedAt: new Date(),
@@ -66,7 +66,7 @@ module.exports = {
              {
                  name:'Proteccion Civil de El Salvador' ,
                  direccion:'Novena Calle Poniente y Quince Avenida Norte, Centro de Gobierno, San Salvador.' ,
-                 email: 'http://proteccioncivil.gob.sv/',
+                 email: 'prensa@proteccioncivil.gob.sv',
 
                  createdAt: new Date(),
                  updatedAt: new Date(),
